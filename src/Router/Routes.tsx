f387648ff6f428bae3118ce01644be4639ff45b4
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-08 13:44:48 -0300

    creating routes

---
+++ src/Router/Routes.tsx
@@ -0,0 +1,46 @@
+import { Routes, Route, Navigate } from "react-router";
+import AppLayout from "../layout/app-layout";
+import ProtectedRoute from "../middleware/auth";
+import Dashboard from "../pages/dashboard";
+import Login from "../pages/auth/login";
+import Register from "../pages/auth/register";
+
+function Router() {
+  const routes = [
+    { path: "/login", element: <Login />, layout: false, isProtected: false },
+    { path: "/register", element: <Register />, layout: false, isProtected: false },
+    { path: "/dashboard", element: <Dashboard />, layout: true, isProtected: true },
+  ];
+
+  return (
+    <Routes>
+      {
+        routes.map(({ path, element, layout, isProtected }) => {
+          let el = element;
+
+          if (isProtected) {
+            el = <ProtectedRoute>{el}</ProtectedRoute>;
+          }
+
+          if (layout) {
+            el = <AppLayout>{el}</AppLayout>;
+          }
+          return (
+            <Route 
+              element={el}
+              key={path}
+              path={path}
+            />
+          )
+        })
+      }
+      <Route 
+        path="*" 
+        element={
+        <Navigate to='/dashboard' replace />
+      } />
+    </Routes>
+  );
+}
+
+export default Router;
