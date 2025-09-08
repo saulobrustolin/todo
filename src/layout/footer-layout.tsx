c29edb57cd686ea2a2174ac2f8302067a4fbb771
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-08 13:43:14 -0300

    npm lucide dev and update footer layout

--- src/layout/footer-layout.tsx
+++ src/layout/footer-layout.tsx
@@ -1,7 +1,23 @@
+import { HouseIcon, Plus } from "lucide-react";
+import Anchor from "../components/Anchor";
+
 function FooterLayout() {
     return (
-        <footer>
-            FooterLayout
+        <footer
+            className="grid grid-cols-2 absolute bottom-0 w-full border-t border-[var(--border-white)]"
+        >
+            <Anchor
+                to="/"
+            >
+                <HouseIcon />
+                Início    
+            </Anchor>
+            <Anchor
+                to="/new"
+            >
+                <Plus />
+                Criar lista
+            </Anchor>
         </footer>
     )
 }
