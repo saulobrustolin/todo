0555b5f93c098f841b5b37f4190b08c09092238b
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-09 14:07:06 -0300

    cloning database struct for diagram to project

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,45 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
+  password: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+  updated_at: timestamp('updated_at', { withTimezone: true }).defaultNow().$onUpdate(() => new Date()),
 });
+
+export const listsTable = pgTable("lists", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  subtitle: varchar({ length: 255 }).notNull(),
+  description: text(),
+  created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+  updated_at: timestamp('updated_at', { withTimezone: true }).defaultNow().$onUpdate(() => new Date()),
+  user_id: integer('user_id').notNull(),
+});
+
+export const listsRelations = relations(listsTable, ({ one }) => ({
+	user: one(usersTable, {
+		fields: [listsTable.user_id],
+		references: [usersTable.id],
+	}),
+}));
+
+export const tasksTable = pgTable("tasks", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  subtitle: varchar({ length: 255 }).notNull(),
+  description: text(),
+  created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+  updated_at: timestamp('updated_at', { withTimezone: true }).defaultNow().$onUpdate(() => new Date()),
+  list_id: integer('list_id').notNull(),
+});
+
+export const tasksRelations = relations(tasksTable, ({ one }) => ({
+  list: one(listsTable, {
+    fields: [tasksTable.list_id],
+    references: [listsTable.id],
+  }),
+}));
