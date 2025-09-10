dba50053299ac44fc44b4a0bdb0886158c215ad9
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-10 17:08:34 -0300

    update Anchor componente with new prop className

--- frontend/src/components/Anchor.tsx
+++ frontend/src/components/Anchor.tsx
@@ -1,8 +1,8 @@
-function Anchor({ to, children }: { to: string, children: React.ReactNode }) {
+function Anchor({ to, children, className }: { to: string, children: React.ReactNode, className?: string }) {
     return (
         <a
             href={to}
-            className="flex flex-col gap-2 justify-center items-center py-4"
+            className={"flex flex-col gap-2 justify-center items-center py-4 " + (className ? className : "")}
         >
             {children}
         </a>
