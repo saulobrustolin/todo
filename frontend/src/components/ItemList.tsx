97ca21aaf79a0d3f01fac5fea8a1f040519bf9e8
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-10 13:17:33 -0300

    update animation to translate-x

--- frontend/src/components/ItemList.tsx
+++ frontend/src/components/ItemList.tsx
@@ -7,7 +7,7 @@ function ItemList({ title, subtitle, id }: { title: string, subtitle: string, id
             className="flex justify-between items-center p-2 py-4 text-lg font-semibold cursor-pointer"
         >
             <Link
-                className="flex flex-col scale-100 hover:scale-[102%] hover:transition-all duration-200 ease-in-out"
+                className="flex flex-col scale-100 translate-x-0 hover:translate-x-1 duration-200 ease-in-out"
                 to={`/list/${id}`}
             >
                 <span>
