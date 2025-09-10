b9d3cba76744e0bc4c7c6501ee01b9675c8ed1d1
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-10 09:47:39 -0300

    get items for list with api request

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,36 +1,62 @@
+import { useEffect, useState } from "react";
 import Collection from "../components/Collection";
 import ItemList from "../components/ItemList";
 import Search from "../components/Search";
+import Skeleton from "../components/Skeleton";
 
 type ItemProp = {
-    name: string
+    id: number,
+    title: string,
+    subtitle: string,
+    description: string | null,
+    created_at: string,
+    updated_at: string,
+    user_id: number
 }
 
 function Dashboard() {
-    const items = [
-        {
-            "name": "Lista de compras"
-        },
-        {
-            "name": "Lista de materiais"
-        },
-        {
-            "name": "Preparação da festa 18/08"
-        },
-    ]
+    const [lists, setLists] = useState<ItemProp[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState();
+
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+        const loadLists = async () => {
+            try {
+                const res = await fetch(`${apiUrl}/lists`);
+                if (!res.ok) throw new Error("Erro na requisição");
+                const data = await res.json();
+                setLists(data["data"]);
+            } catch (err: any) {
+                if (err.name !== "AbortError") setError(err.message)
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadLists();
+    }, [])    
 
     return (
         <>
             <Search/>
             <Collection>
             {
-                items.map((value: ItemProp, index: number) => {
-                    return (
-                        <ItemList
-                            key={index}
-                        >{value.name}</ItemList>
-                    )
-                })
+                lists.length != 0 ? (
+                    lists.map((value: ItemProp) => {
+                        return (
+                            <ItemList
+                                key={value.id}
+                                title={value.title}
+                                subtitle={value.subtitle}
+                            />
+                        )
+                    })
+                ) : (
+                    <Skeleton 
+                        className="w-full h-16"
+                    />
+                )
             }
             </Collection>
         </>
