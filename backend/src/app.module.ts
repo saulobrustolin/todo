d7dd8c86e69d2dfb92cb2afa5eba39576d3293ba
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-10 09:49:09 -0300

    revert: app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ListController } from './list/list.controller';
+import { ListService } from './list/list.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ListController],
+  providers: [AppService, ListService],
 })
 export class AppModule {}
