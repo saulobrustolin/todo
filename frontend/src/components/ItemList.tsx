ee3f512eb55d0f5196996cb3ba6f9f71cc9fe685
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-10 10:46:17 -0300

    create animation dinamic for items in list

--- frontend/src/components/ItemList.tsx
+++ frontend/src/components/ItemList.tsx
@@ -3,10 +3,10 @@ import { ChevronRight } from "lucide-react";
 function ItemList({ title, subtitle }: { title: string, subtitle: string }) {
     return (
         <div
-            className="flex justify-between items-center p-2 py-4 text-lg font-semibold cursor-pointer hover:transform-cpu hover:transition-all duration-500 ease-in-out"
+            className="flex justify-between items-center p-2 py-4 text-lg font-semibold cursor-pointer"
         >
             <div
-                className="flex flex-col"
+                className="flex flex-col scale-100 hover:scale-[102%] hover:transition-all duration-200 ease-in-out"
             >
                 <span>
                     {title}
