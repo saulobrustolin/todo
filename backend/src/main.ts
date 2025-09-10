c37d00e81bdbabe701b7141630efb2fb19f0313e
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-10 09:43:24 -0300

    habilited cors in api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-const db = drizzle(process.env.DATABASE_URL!);
-console.log(db);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
