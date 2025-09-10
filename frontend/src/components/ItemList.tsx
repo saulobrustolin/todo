8aad51eeea4138b5d2a36f620db292f07107dbee
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-10 09:50:21 -0300

    component ItemList with styles and conditions for render

--- frontend/src/components/ItemList.tsx
+++ frontend/src/components/ItemList.tsx
@@ -1,11 +1,26 @@
 import { ChevronRight } from "lucide-react";
 
-function ItemList({ children }: { children: React.ReactNode }) {
+function ItemList({ title, subtitle }: { title: string, subtitle: string }) {
     return (
         <div
             className="flex justify-between items-center p-2 py-4 text-lg font-semibold cursor-pointer hover:transform-cpu hover:transition-all duration-500 ease-in-out"
         >
-            {children}
+            <div
+                className="flex flex-col"
+            >
+                <span>
+                    {title}
+                </span>
+                {
+                    subtitle ? (
+                        <span
+                        className="text-[var(--secondary-text)] font-normal text-sm"
+                        >
+                            {subtitle}
+                        </span>
+                    ) : null
+                }
+            </div>
             <ChevronRight />
         </div>
     )
