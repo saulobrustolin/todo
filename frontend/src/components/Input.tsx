1aad1aa929f402fa2062eaf108ac61f4e172e99b
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-11 17:21:50 -0300

    update Input component

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -5,25 +5,30 @@ type InputProps = {
     name: string,
     disabled?: boolean,
     value?: string,
-    change?: (value: string) => void;
+    change?: (value: string) => void,
+    className?: string
 }
 
-function Input({ type, label, placeholder, name, disabled=false, value, change }: InputProps) {
+function Input({ type, label, placeholder, name, disabled=false, value, change, className }: InputProps) {
     return (
         <div
             className={"text-black flex flex-col " + (disabled ? "hidden" : "")}
         >
-            <label 
-                htmlFor={`${label}-${type}`}
-                className="text-md font-semibold cursor-pointer"
-            >
-                {label}
-            </label>
+            {
+                label ? (
+                    <label 
+                        htmlFor={`${label}-${type}`}
+                        className={"text-md font-semibold cursor-pointer " + (className ? "text-[var(--primary-text)]" : "")}
+                    >
+                        {label}
+                    </label>
+                ) : null
+            }
             <input 
                 id={`${label}-${type}`} 
                 type={type ? type : "text"} 
                 placeholder={placeholder}
-                className="focus:outline-0 text-lg"
+                className={className ? className : "focus:outline-0 text-lg"}
                 name={name}
                 value={value}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => change?.(e.target.value)}
