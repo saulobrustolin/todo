0d28064db0dee95a2b5af9448a8b8d165dba8e37
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-11 14:38:17 -0300

    update style in Skeleton component

--- frontend/src/components/Skeleton.tsx
+++ frontend/src/components/Skeleton.tsx
@@ -1,9 +1,9 @@
 function Skeleton({ className }: { className: string }) {
     return (
-        <div className="mx-auto w-full max-w-sm rounded-md border border-blue-300 p-4">
+        <div className="mx-auto w-full max-w-sm rounded-md border border-stone-800/50 p-2">
             <div className="flex animate-pulse space-x-4">
                 <div
-                    className={`bg-gray-200 ${className}`}
+                    className={`bg-stone-800/50 ${className}`}
                 />
             </div>
         </div>
