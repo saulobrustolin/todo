a76ee1a4d8ad0c5dbd239d8c85ec3dcc5aefff30
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-11 08:37:40 -0300

    update Input component with styles and actions

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,7 +1,21 @@
-function Input() {
+function Input({ type, label, placeholder, name }: { type?: string, label: string, placeholder: string, name: string }) {
     return (
-        <div>
-            Input
+        <div
+            className="text-black flex flex-col"
+        >
+            <label 
+                htmlFor={`${label}-${type}`}
+                className="text-md font-semibold cursor-pointer"
+            >
+                {label}
+            </label>
+            <input 
+                id={`${label}-${type}`} 
+                type={type ? type : "text"} 
+                placeholder={placeholder}
+                className="focus:outline-0 text-lg"
+                name={name}
+            />
         </div>
     )
 }
