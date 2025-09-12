7b6c4255f229640c1c185fdede2ce66e45dc2e7a
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-12 17:22:13 -0300

    adding gap in distance label->textarea

--- frontend/src/components/Textarea.tsx
+++ frontend/src/components/Textarea.tsx
@@ -3,24 +3,25 @@ type TextareaProps = {
     label: string,
     placeholder: string,
     value?: string,
-    change?: (value: string) => void
+    change?: (value: string) => void,
+    className?: string
 }
 
-function Textarea({ name, label, placeholder, value, change }: TextareaProps) {
+function Textarea({ name, label, placeholder, value, change, className }: TextareaProps) {
     return (
         <div
-            className="text-black flex flex-col"
+            className="text-black flex flex-col gap-2"
         >
             <label 
                 htmlFor={`${label}-${name}`}
-                className="text-md font-semibold cursor-pointer"
+                className={"text-md font-semibold cursor-pointer " + (className ? "text-[var(--primary-text)]" : "")}
             >
                 {label}
             </label>
             <textarea 
                 name={name}
                 id={`${label}-${name}`}
-                className="focus:outline-0 resize-none text-lg"
+                className={"focus:outline-0 resize-none text-lg " + (className ? className : "")}
                 placeholder={placeholder}
                 rows={4}
                 value={value}
