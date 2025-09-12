a6207600a7d3d81fefa8c403e022684264839814
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-12 17:18:39 -0300

    create UserService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,12 @@ import { ListController } from './list/list.controller';
 import { ListService } from './list/list.service';
 import { TaskController } from './task/task.controller';
 import { TaskService } from './task/task.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, ListController, TaskController],
-  providers: [AppService, ListService, TaskService],
+  controllers: [AppController, ListController, TaskController, UserController],
+  providers: [AppService, ListService, TaskService, UserService],
 })
 export class AppModule {}
