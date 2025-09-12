9ca9bd8a31118992c52b70f5992f5b69db23137a
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-09-12 17:17:20 -0300

    adding type in input

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,8 +1,16 @@
-function Button({ children, className, handleClick }: { children: React.ReactNode, className?: string, handleClick: any }) {
+type ButtonProps = {
+    children: React.ReactNode,
+    className?: string,
+    handleClick?: any,
+    type?: "submit" | "reset" | "button"
+}
+
+function Button({ children, className, handleClick, type='button' }: ButtonProps) {
     return (
         <button
             className={"rounded flex justify-center p-2 cursor-pointer scale-100 hover:scale-[101%] " + (className)}
             onClick={handleClick}
+            type={type}
         >
             {children}
         </button>
